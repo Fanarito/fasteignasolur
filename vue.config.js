4ce04daa81cd9388f84f0104b5d21be0795ce2ea
Viktor Sævarsson <viktor.saevars@gmail.com>
2019-01-14 14:50:52 +0000

    Added TailwindExtractor to purge css extractors

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ const purgeCssPlugin = require('purgecss-webpack-plugin');
 const glob = require('glob-all');
 const path = require('path');
 
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   configureWebpack: {
     // Merged into the final Webpack config
@@ -12,6 +22,13 @@ module.exports = {
           path.join(__dirname, './**/*.vue'),
           path.join(__dirname, './src/**/*.js'),
         ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js', 'vue'],
+          },
+        ],
+        whitelist: ['html'],
       }),
     ],
   },
