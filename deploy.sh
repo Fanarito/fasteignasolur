4fa021c8ae8fb654ba036174aa75bd08fa8a6f84
Viktor Sævarsson <viktor.saevars@gmail.com>
2019-01-31 01:24:06 +0000

    Set cache headers in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,12 @@ for x in $(find tmp -name \*.gz); do
   mv $x $(echo "$x" | sed 's/\.gz$//')
 done
 
-echo "Cleaning bucket"
-aws s3 rm s3://$BUCKET --recursive
 echo "Uploading files"
-aws s3 sync tmp/dist s3://$BUCKET --acl public-read
-echo "Set correct encoding on css and js files"
-aws s3 cp --recursive s3://$BUCKET/js s3://$BUCKET/js --metadata-directive REPLACE --content-encoding 'gzip'
-aws s3 cp --recursive s3://$BUCKET/css s3://$BUCKET/css --metadata-directive REPLACE --content-encoding 'gzip'
+aws s3 sync --delete tmp/dist s3://$BUCKET --acl public-read
+echo "Set headers on files"
+aws s3 cp --recursive s3://$BUCKET/js s3://$BUCKET/js --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=86400'
+aws s3 cp --recursive s3://$BUCKET/css s3://$BUCKET/css --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=86400'
+aws s3 cp --recursive s3://$BUCKET/img s3://$BUCKET/img --metadata-directive REPLACE --cache-control 'max-age=604800'
 
 echo "Cleaning up temporary folder"
 rm -r tmp
