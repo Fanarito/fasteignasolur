de25d69a818d30e0b78f660a5d498a3a31d7e9e0
Viktor Sævarsson <viktor.saevars@gmail.com>
2019-02-05 11:54:15 +0000

    Bætti við Eignaver

---
+++ src/domain/sellers/EignaverSeller.ts
@@ -0,0 +1,27 @@
+import AbstractSeller from './AbstractSeller';
+import Seller from './Seller';
+import Fee from '../fees/Fee';
+import SalesPercentageFee from '../fees/SalesPercentageFee';
+import HourlyFee from '../fees/HourlyFee';
+import FlatFee from '../fees/FlatFee';
+
+export default class EignaverSeller extends AbstractSeller implements Seller {
+  public name = 'Eignaver';
+  public logoUrl = 'img/sellers/eignaver.png';
+  public color = '#ff1803';
+
+  public feesTaken(price: number, hoursWorked: number): Fee[] {
+    return [
+      new SalesPercentageFee('í söluþóknun', price, 0.02356, 396_400),
+      new FlatFee('fyrir gagnaöflun', 43_400, 'Gagnaöflun'),
+      new HourlyFee(24_800, hoursWorked),
+      new SalesPercentageFee(
+        'fyrir aðstoð og/eða skjalafrágang',
+        price,
+        0.01,
+        248_000,
+        'Skjalafrágangur',
+      ),
+    ];
+  }
+}
--- src/domain/sellers/sellers.ts
+++ src/domain/sellers/sellers.ts
@@ -7,10 +7,12 @@ import HeimiliSeller from './HeimiliSeller';
 import LandmarkSeller from './Landmark';
 import MidlunSeller from './MidlunSeller';
 import NyttHeimiliSeller from './NyttHeimiliSeller';
+import EignaverSeller from './EignaverSeller';
 
 export default [
   new BorgSeller(),
   new EignarmidlunSeller(),
+  new EignaverSeller(),
   new HeimiliSeller(),
   new LandmarkSeller(),
   new LindSeller(),
