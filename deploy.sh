b0977c4b51ee2a91f7c6b39b0024c9c737559a67
Viktor Sævarsson <viktor.saevars@gmail.com>
2019-02-20 14:06:39 +0000

    Hækkaði cache tíman á öllum assettum í 1 ár

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ aws s3 rm s3://$BUCKET --recursive
 echo "Uploading files"
 aws s3 sync tmp/dist s3://$BUCKET --acl public-read
 echo "Set headers on files"
-aws s3 cp --recursive s3://$BUCKET/js s3://$BUCKET/js --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=86400'
-aws s3 cp --recursive s3://$BUCKET/css s3://$BUCKET/css --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=86400'
-aws s3 cp --recursive s3://$BUCKET/img s3://$BUCKET/img --metadata-directive REPLACE --cache-control 'max-age=604800'
+aws s3 cp --recursive s3://$BUCKET/js s3://$BUCKET/js --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=31536000'
+aws s3 cp --recursive s3://$BUCKET/css s3://$BUCKET/css --metadata-directive REPLACE --content-encoding 'gzip' --cache-control 'max-age=31536000'
+aws s3 cp --recursive s3://$BUCKET/img s3://$BUCKET/img --metadata-directive REPLACE --cache-control 'max-age=31536000'
 
 echo "Cleaning up temporary folder"
 rm -r tmp
