a78d6fe2ac2ed517cacf41083276a21cd3050f96
Viktor Sævarsson <viktor@vsa.is>
2023-11-01 23:58:59 +0100

    Update Eignaver tariff url

--- src/domain/sellers/EignaverSeller.ts
+++ src/domain/sellers/EignaverSeller.ts
@@ -8,11 +8,11 @@ export default class EignaverSeller extends AbstractSeller implements Seller {
   public name = 'Eignaver';
   public logoUrl = 'img/sellers/eignaver.png';
   public color = '#ff1803';
-  public tariffUrl = 'https://www.eignaver.is/text2/';
+  public tariffUrl = 'https://www.eignaver.is/gjaldskra';
 
   public feesTaken(price: number, hoursWorked: number): Fee[] {
     return [
-      new SalesPercentageFee('í söluþóknun', price, 0.0195, 477_400),
+      new SalesPercentageFee('í söluþóknun', price, 0.0195, 489_800),
       new FlatFee('fyrir gagnaöflun', 43_400),
     ];
   }
